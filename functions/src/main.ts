221140efa6a7ee8f1751ca89bc50b2b884aa585d
dev-marian-vybyranyi <marian.vybyranyi.dev@gmail.com>
2026-02-13 10:35:05 +0200

    Adapt NestJS application for Firebase Functions, enabling CORS, updating runtime to Node.js 20, and adjusting emulator port.

--- functions/src/main.ts
+++ functions/src/main.ts
@@ -1,8 +1,28 @@
 import { NestFactory } from '@nestjs/core';
+import { ExpressAdapter } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
+import express from 'express';
+import * as functions from 'firebase-functions';
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
-}
-bootstrap();
+const server = express();
+
+export const createNestServer = async (expressInstance: express.Express) => {
+  const app = await NestFactory.create(
+    AppModule,
+    new ExpressAdapter(expressInstance),
+  );
+  
+  app.enableCors({
+    origin: 'http://localhost:5173',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
+  
+  await app.init();
+};
+
+createNestServer(server)
+  .then(() => console.log('Nest Ready'))
+  .catch((err) => console.error('Nest broken', err));
+
+export const api = functions.https.onRequest(server);
