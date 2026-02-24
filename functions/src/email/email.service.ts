47a3f0056b2deca74e910f264ac40ceb1a475daa
dev-marian-vybyranyi <marian.vybyranyi.dev@gmail.com>
2026-02-24 10:21:26 +0200

    switch email transporter configuration from Mailtrap to Resend.

--- functions/src/email/email.service.ts
+++ functions/src/email/email.service.ts
@@ -8,11 +8,11 @@ export class EmailService {
 
   constructor() {
     this.transporter = nodemailer.createTransport({
-      host: process.env.MAILTRAP_HOST || 'sandbox.smtp.mailtrap.io',
-      port: Number(process.env.MAILTRAP_PORT) || 2525,
+      host: process.env.RESEND_HOST,
+      port: Number(process.env.RESEND_PORT),
       auth: {
-        user: process.env.MAILTRAP_USER,
-        pass: process.env.MAILTRAP_PASS,
+        user: process.env.RESEND_USER,
+        pass: process.env.RESEND_PASS,
       },
     });
   }
